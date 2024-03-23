bc4e2838647c2960c0dc71fd0ef7ad14e4af8ad1
Daniel González Briseño <qbixmex@gmail.com>
2024-03-23 14:11:45 -0700

    fonts-configuration
    - [x] Fonts Configured
    - [x] Home Page Modified

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
+import { titleFont, paragraph } from '@/config/fonts';
 import styles from './home.module.css';
+
+const headingClasses = `${titleFont.className} ${styles.heading}`;
+const paragraphClasses = `${paragraph.className} ${styles.paragraph}`;
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1 className={styles.heading}>
+      <h1 className={headingClasses}>
         Welcome to Teslo Shop
       </h1>
-
-      <p className={styles.content}>This website is under construction</p>
+      <p className={paragraphClasses}>
+        This website is under construction
+      </p>
     </main>
   );
 }
---
+++ src/config/fonts.ts
@@ -0,0 +1,14 @@
+import {
+  Inter,
+  Montserrat_Alternates
+} from "next/font/google";
+
+export const titleFont = Montserrat_Alternates({
+  subsets: ["latin"],
+  weight: ['500', '700']
+});
+
+export const paragraph = Inter({
+  subsets: ["latin"],
+  weight: ['500']
+});
