36703dbf48aea176c56c1e4400da16f7cb2da6dd
Daniel González Briseño <qbixmex@gmail.com>
2024-03-23 13:38:40 -0700

    initial-setup
    - [x] Inital Configuration.
    - [x] Assets Added (product / images).
    - [x] Removed code.

---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import styles from './home.module.css';
+
+export default function Home() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.heading}>
+        Welcome to Teslo Shop
+      </h1>
+
+      <p className={styles.content}>This website is under construction</p>
+    </main>
+  );
+}
