de460db79499a17da7aaffbf99f179c578627143
Daniel González Briseño <qbixmex@gmail.com>
2024-03-30 17:46:10 -0700

    prisma-schema

---
+++ prisma/migrations/20240331003404_init/migration.sql
@@ -0,0 +1,45 @@
+-- CreateEnum
+CREATE TYPE "Size" AS ENUM ('XS', 'S', 'M', 'L', 'XL', 'XXL', 'XXXL');
+
+-- CreateEnum
+CREATE TYPE "Gender" AS ENUM ('men', 'women', 'kid', 'unisex');
+
+-- CreateTable
+CREATE TABLE "categories" (
+    "id" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "categories_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "products" (
+    "id" TEXT NOT NULL,
+    "title" VARCHAR(150) NOT NULL,
+    "slug" TEXT NOT NULL,
+    "description" VARCHAR(150) NOT NULL,
+    "in_stock" INTEGER DEFAULT 0,
+    "price" DOUBLE PRECISION DEFAULT 0,
+    "size" "Size"[] DEFAULT ARRAY[]::"Size"[],
+    "tags" TEXT[] DEFAULT ARRAY[]::TEXT[],
+    "gender" "Gender" NOT NULL,
+    "category_id" TEXT NOT NULL,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "products_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "categories_name_key" ON "categories"("name");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "products_slug_key" ON "products"("slug");
+
+-- CreateIndex
+CREATE INDEX "products_title_gender_idx" ON "products"("title", "gender");
+
+-- AddForeignKey
+ALTER TABLE "products" ADD CONSTRAINT "products_category_id_fkey" FOREIGN KEY ("category_id") REFERENCES "categories"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
