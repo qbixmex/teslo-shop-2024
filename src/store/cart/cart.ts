7be6cda8d805e023f5cda1c91068a2c8d3086c89
Daniel González Briseño <qbixmex@gmail.com>
2024-06-25 13:29:48 -0700

    Add to cart store method

--- src/store/cart/cart.ts
+++ src/store/cart/cart.ts
@@ -3,13 +3,41 @@ import { create } from 'zustand';
 
 type State = {
   cart: CartProduct[];
-  // addProduct
+  addProduct: (product: CartProduct) => void,
   // updateProductQuantity
   // removeProduct
 };
 
 export const useCartStore = create<State>()(
-  (set) => ({
+  (set, get) => ({
     cart: [],
+
+    // Methods
+    addProduct: (product) => {
+      const { cart } = get();
+      // 1. Check if product is already in cart with selected size.
+      const productInCart = cart.some(
+        (item) => ((item.id === product.id) && (item.size === product.size))
+      );
+
+      if (!productInCart) {
+        set({ cart: [ ...cart, product ]});
+        return;
+      }
+
+      // 2. If product is already in cart, update the quantity.
+      const updatedCartProducts = cart.map((item) => {
+        if ((item.id === product.id) && (item.size === product.size)) {
+          return {
+            ...item,
+            quantity: item.quantity + product.quantity,
+          };
+        }
+
+        return item;
+      });
+
+      set({ cart: updatedCartProducts });
+    },
   })
 );
