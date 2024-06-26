9f4469d1cc752ef00cd28d9cb35c53563043ba62
Daniel González Briseño <qbixmex@gmail.com>
2024-06-26 14:30:37 -0700

    Credentials Provider

--- src/auth.config.ts
+++ src/auth.config.ts
@@ -1,9 +1,41 @@
-import type { NextAuthConfig } from 'next-auth';
- 
+import NextAuth, { type NextAuthConfig } from 'next-auth';
+import Credentials from 'next-auth/providers/credentials';
+import { z } from 'zod';
+
 export const authConfig: NextAuthConfig = {
   pages: {
     signIn: '/auth/login',
     newUser: '/auth/new-account',
   },
-  providers: []
+  providers: [
+    Credentials({
+      async authorize(credentials) {
+        const parsedCredentials = z
+          .object({
+            email: z.string().email(),
+            password: z.string().min(6)
+          })
+          .safeParse(credentials);
+        
+        if (!parsedCredentials.success) {
+          return null;
+        }
+
+        const { email, password } = parsedCredentials.data;
+
+        console.log("=============CREDENTIALS=============");
+        console.log("EMAIL:", email);
+        console.log("PASSWORD:", password);
+        console.log("=====================================");
+
+        // 1. Search email.
+        // 2. Compare passwords.
+        // 3. Return user.
+
+        return null;
+      },
+    }),
+  ]
 };
+
+export const { signIn, signOut, auth } = NextAuth(authConfig);
