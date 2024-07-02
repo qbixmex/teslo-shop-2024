47450c9bb159d8b1c493c6e96e7b2cb7582e9e7d
Daniel González Briseño <qbixmex@gmail.com>
2024-07-02 01:57:26 -0700

    Server Action - Verify Paypal Payment

--- src/actions/payments/paypal-check-payment.ts
+++ src/actions/payments/paypal-check-payment.ts
@@ -1,5 +1,7 @@
 'use server';
 
+import { PaypalOAuthResponse, PayPalOrderStatusResponse } from "@/interfaces";
+
 const paypalCheckPayment = async ( paypalTransactionId: string ) => {
   console.log({paypalTransactionId});
 
@@ -13,15 +15,29 @@ const paypalCheckPayment = async ( paypalTransactionId: string ) => {
       message: "Failed to get PayPal Token !",
     };
   }
-};
 
-type PaypalOAuthResponse = {
-  scope: string;
-  access_token: string;
-  token_type: string;
-  app_id: string;
-  expires_in: number;
-  nonce: string;
+  const orderStatus = await verifyPayPalPayment(paypalTransactionId, authToken);
+
+  if (!orderStatus) {
+    return {
+      ok: false,
+      message: "Failed to verify PayPal Payment !",
+    };
+  }
+
+  const { status, purchase_units } = orderStatus;
+  // TODO: Invoice ID
+  // const {  } = purchase_units[0]; 
+
+  if (status !== "COMPLETED") {
+    return {
+      ok: false,
+      message: "Payment is not completed yet !",
+    };
+  }
+
+  // TODO: Update database with the payment status.
+
 };
 
 /**
@@ -59,6 +75,41 @@ const getPayPalBearerToken = async (): Promise<string | null> => {
     const response = await fetch(PAYPAL_OAUTH_URL, requestOptions);
     const { access_token } = await response.json() as PaypalOAuthResponse;
     return access_token;
+  } catch (error) {
+    console.error(error);
+    return null;
+  }
+};
+
+/**
+ * Verify PayPal Payment and get Order Status.
+ * @param paypalTransactionId PayPal Transaction ID.
+ * @param token Bearer Token.
+ * @returns PayPal Order Status Response or null.
+ */
+const verifyPayPalPayment = async (
+  paypalTransactionId: string,
+  token: string
+): Promise<PayPalOrderStatusResponse | null> => {
+  const PAYPAL_ORDERS_URL = process.env.PAYPAL_ORDERS_URL ?? '';
+  const customHeaders = new Headers();
+  customHeaders.append(
+    // Important: Space after "Bearer" is required !
+    "Authorization", "Bearer" + " " + token
+  );
+  
+  const requestOptions = {
+    method: "GET",
+    headers: customHeaders,
+  };
+  
+  const URL = `${PAYPAL_ORDERS_URL}/${paypalTransactionId}`;
+
+  try {
+    const response = await fetch(URL, requestOptions);
+    const data = await response.json() as PayPalOrderStatusResponse;
+    console.log("DATA:", data);
+    return data;
   } catch (error) {
     console.error(error);
     return null;
--- src/interfaces/index.ts
+++ src/interfaces/index.ts
@@ -1,4 +1,5 @@
 export * from './address.interface';
 export * from './country.interface';
 export * from './order.interface';
+export * from './paypal.interfaces';
 export * from './product.interface';
