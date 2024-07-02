296130f1705fccc38739e1ae352e2243a106926e
Daniel González Briseño <qbixmex@gmail.com>
2024-07-01 20:43:17 -0700

    Show PayPal Skeleton

--- src/components/paypal/paypal.button.tsx
+++ src/components/paypal/paypal.button.tsx
@@ -1,13 +1,26 @@
 'use client';
 
 import { FC } from 'react';
-import { PayPalButtons } from '@paypal/react-paypal-js';
+import { PayPalButtons, usePayPalScriptReducer } from '@paypal/react-paypal-js';
+import styles from './paypal.button.module.css';
 
 type Props = {
   layout?: 'horizontal' | 'vertical';
 };
 
 const PaypalButton: FC<Props> = ({ layout = 'horizontal' }) => {
+
+  const [{ isPending }] = usePayPalScriptReducer();
+
+  if (!isPending) {
+    return (
+      <div className={styles.skeletonContainer}>
+        <div className={styles.skeleton} />
+        <div className={styles.skeleton} />
+      </div>
+    );
+  }
+
   return (
     <PayPalButtons style={{ layout }} />
   );
